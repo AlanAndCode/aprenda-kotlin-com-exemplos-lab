e4d8bd2e56dfc70fe654e47db1669710943e16f1
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-08-20 01:21:08 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,46 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Aluno(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
+    val nivel: Nivel = gerarNivelAleatorio()
+    val alunosMatriculados = mutableListOf<Aluno>()
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(aluno: Aluno) {
+        alunosMatriculados.add(aluno)
+    }
+
+    private fun gerarNivelAleatorio(): Nivel {
+        val valoresNiveis = Nivel.values()
+        val indiceAleatorio = Random.nextInt(valoresNiveis.size)
+        return valoresNiveis[indiceAleatorio]
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criar alguns objetos de ConteudoEducacional
+    val conteudo1 = ConteudoEducacional("Kotlin desenvolvimento de apps")
+    val conteudo2 = ConteudoEducacional("programaçao de apps")
+
+
+    // Criar uma formação e adicionar conteúdos
+    val formacao = Formacao("Desenvolvimento Android", listOf(conteudo1, conteudo2))
+
+    // Criar alguns alunos
+    val aluno1 = Aluno("Alan")
+    val aluno2 = Aluno("Modesto")
+
+
+    // Matricular os alunos na formação
+    formacao.matricular(aluno1)
+    formacao.matricular(aluno2)
+
+    // Verificar os alunos matriculados na formação
+    println("Alunos matriculados na formação ${formacao.nome} de nível ${formacao.nivel}:")
+    for (aluno in formacao.alunosMatriculados) {
+        println(aluno.nome)
+    }
 }
